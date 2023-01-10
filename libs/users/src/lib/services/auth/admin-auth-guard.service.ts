93c2e3dd3f5ad4bc171730e6a200cae7470066af
Abbas Ghaith <abbasghayth5@gmail.com>
2023-01-10 23:05:32 +0200

    Fix authentication bug

--- libs/users/src/lib/services/auth/admin-auth-guard.service.ts
+++ libs/users/src/lib/services/auth/admin-auth-guard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { LocalstorageService } from './localstorage.service';
 import { _tokenExpired } from './common.services';
+import { MessageService } from 'primeng/api';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +11,7 @@ export class AdminAuthGuard implements CanActivate {
   constructor(
     private router: Router,
     private localstorageService: LocalstorageService,
+    private messageService: MessageService,
   ) { }
 
   // eslint-disable-next-line
@@ -17,11 +19,17 @@ export class AdminAuthGuard implements CanActivate {
     const token = this.localstorageService.getToken();
     if (token) {
       const tokenDecode = JSON.parse(atob(token.split('.')[1]));
-      if (tokenDecode.isAdmin && !_tokenExpired(tokenDecode.Exp)) return true;
+      if (tokenDecode.isAdmin && !_tokenExpired(tokenDecode.Exp)) {
+        return true
+      } else {
+        this.messageService.add({
+          severity:'error', 
+          summary:'Error', 
+          detail:'Woops! You are not allowed to access the admin panel'
+        });
+      }
     }
-
     this.router.navigate(['/login']);
-    
     return false;
   }
 }
